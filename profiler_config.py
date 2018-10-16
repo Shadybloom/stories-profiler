0a3075713336c5106bcd5fce2a02f39eb1e781d6
Shadybloom Applepie <celestia@safe-mail.net>
2018-10-16 21:33:43 +0300

    Правки конфига.

--- profiler_config.py
+++ profiler_config.py
@@ -1,7 +1,9 @@
 BOOKS_DIR = '.data/ponyfiction_fb2'
+DATABASE_DIR = 'database'
+TOKENS_DICT = 'database/tokens.pickle'
 DATABASE_PATH = 'database/stories.sqlite'
 # Нормализация слов с помощью pymorphy:
-MORPHY_SOFT = False
+MORPHY_SOFT = True
 MORPHY_FORCED = False
 # Хранить текст не обязательно, достаточно списков:
 SAVE_BOOK_TEXT = False
@@ -14,3 +16,5 @@ SCORE_MIN = 5
 SCORE_MAX = 5000
 # С каждым новым текстом в бд таблица всё равно пересоздаётся:
 regen_words_table = False
+# Строк в выводе:
+OUTPUT_LINES = 20
